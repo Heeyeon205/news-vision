d7761988ffb717e22746599dc781f5b1f0cdd0e8
Songmyungbo <audqh656@gmail.com>
2025-04-29 09:46:33 +0900

    feat:0429 투표 수정

--- src/main/java/com/newsvision/poll/repository/PollOptionRepository.java
+++ src/main/java/com/newsvision/poll/repository/PollOptionRepository.java
@@ -4,4 +4,5 @@ import com.newsvision.poll.entity.PollOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PollOptionRepository extends JpaRepository<PollOption, Long> {
+    void deleteByPollId(Long pollId);
 }
--- src/main/java/com/newsvision/poll/repository/PollRepository.java
+++ src/main/java/com/newsvision/poll/repository/PollRepository.java
@@ -1,7 +1,12 @@
 package com.newsvision.poll.repository;
 
 import com.newsvision.poll.entity.Poll;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PollRepository extends JpaRepository<Poll, Long> {
+    @EntityGraph(attributePaths = {"user", "pollOptions"})
+    Optional<Poll> findById(Long id);
 }
