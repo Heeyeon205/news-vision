0c67cc123d363c24ccee62d7c2f88d060a88a745
jiwon0324 <wonjj0324@gmail.com>
2025-04-29 09:46:34 +0900

    feat:없음
    없음

--- src/test/java/com/newsvision/NewsVisionApplicationTests.java
+++ src/test/java/com/newsvision/NewsVisionApplicationTests.java
@@ -1,13 +1,13 @@
 package com.newsvision;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NewsVisionApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NewsVisionApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
