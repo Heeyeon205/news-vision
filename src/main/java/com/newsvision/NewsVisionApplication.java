b8c7f4f474833066c962503c246931f6201380b5
Lyle <procross27@gmail.com>
2025-05-07 00:32:20 +0900

    Update NewsVisionApplication.java

--- src/main/java/com/newsvision/NewsVisionApplication.java
+++ src/main/java/com/newsvision/NewsVisionApplication.java
@@ -3,11 +3,10 @@ package com.newsvision;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableElasticsearchRepositories(basePackages = "com.newsvision.elasticsearch.repository")
+@SpringBootApplication
 public class NewsVisionApplication {
 
     public static void main(String[] args) {
